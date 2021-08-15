6cc9bfd452240ad1f2e017c7dcbfc52bfc045364
Joe Dean <jpd62@cam.ac.uk>
2021-08-15 16:14:24 +0100

    Begin assemble matrix

---
+++ python/dolfinx_hdg/assemble.py
@@ -0,0 +1,37 @@
+import functools
+from petsc4py import PETSc
+import dolfinx
+import typing
+from dolfinx.fem.dirichletbc import DirichletBC
+from dolfinx.fem.form import Form
+from dolfinx.fem.assemble import _create_cpp_form
+
+@functools.singledispatch
+def assemble_matrix(a: typing.Union[Form, dolfinx.cpp.fem.Form],
+                    bcs: typing.List[DirichletBC] = [],
+                    diagonal: float = 1.0) -> PETSc.Mat:
+    """Assemble bilinear form into a matrix. The returned matrix is not
+    finalised, i.e. ghost values are not accumulated.
+
+    """
+    # A = cpp.fem.create_matrix(_create_cpp_form(a))
+    print("Hi")
+    # return assemble_matrix(A, a, bcs, diagonal)
+
+
+@assemble_matrix.register(PETSc.Mat)
+def _(A: PETSc.Mat,
+      a: typing.Union[Form, dolfinx.cpp.fem.Form],
+      bcs: typing.List[DirichletBC] = [],
+      diagonal: float = 1.0) -> PETSc.Mat:
+    """Assemble bilinear form into a matrix. The returned matrix is not
+    finalised, i.e. ghost values are not accumulated.
+
+    """
+    _a = _create_cpp_form(a)
+    dolfinx.cpp.fem.assemble_matrix_petsc(A, _a, bcs)
+    if _a.function_spaces[0].id == _a.function_spaces[1].id:
+        A.assemblyBegin(PETSc.Mat.AssemblyType.FLUSH)
+        A.assemblyEnd(PETSc.Mat.AssemblyType.FLUSH)
+        dolfinx.cpp.fem.insert_diagonal(A, _a.function_spaces[0], bcs, diagonal)
+    return A
---
+++ python/test/test_assemble_matrix.py
@@ -0,0 +1,22 @@
+from dolfinx import UnitSquareMesh, FunctionSpace
+from mpi4py import MPI
+from ufl import TrialFunction, TestFunction, inner, dx, ds
+from dolfinx_hdg.assemble import assemble_matrix
+
+n = 1
+mesh = UnitSquareMesh(MPI.COMM_WORLD, n, n)
+
+V = FunctionSpace(mesh, ("DG", 1))
+Vbar = FunctionSpace(mesh, ("DG", 1), codimension=1)
+
+u = TrialFunction(V)
+v = TestFunction(V)
+ubar = TrialFunction(Vbar)
+vbar = TestFunction(Vbar)
+
+a00 = inner(u, v) * dx + inner(u, v) * ds
+a10 = inner(u, vbar) * ds
+a01 = inner(v, ubar) * ds
+a11 = inner(ubar, vbar) * dx
+
+A = assemble_matrix(a00)
