f188dbf21feac8410a5c36cf1ff6a41ce507cb5c
Joe Dean <jpd62@cam.ac.uk>
2021-08-16 17:06:24 +0100

    Get data

--- cpp/assemble_matrix_impl.h
+++ cpp/assemble_matrix_impl.h
@@ -35,6 +35,37 @@ namespace dolfinx_hdg::fem::impl
         const dolfinx::array2d<T> &coeffs, const std::vector<bool> &bc0,
         const std::vector<bool> &bc1)
     {
-        std::cout << "Hello from dolfinx_hdg::fem::impl::assemble_matrix\n";
+        std::shared_ptr<const dolfinx::mesh::Mesh> mesh = a.mesh();
+        assert(mesh);
+        const int tdim = mesh->topology().dim();
+
+        // Get dofmap data
+        std::shared_ptr<const dolfinx::fem::DofMap> dofmap0 = a.function_spaces().at(0)->dofmap();
+        std::shared_ptr<const dolfinx::fem::DofMap> dofmap1 = a.function_spaces().at(1)->dofmap();
+        assert(dofmap0);
+        assert(dofmap1);
+        const dolfinx::graph::AdjacencyList<std::int32_t> &dofs0 = dofmap0->list();
+        const int bs0 = dofmap0->bs();
+        const dolfinx::graph::AdjacencyList<std::int32_t> &dofs1 = dofmap1->list();
+        const int bs1 = dofmap1->bs();
+
+        std::shared_ptr<const dolfinx::fem::FiniteElement> element0 = a.function_spaces().at(0)->element();
+        std::shared_ptr<const dolfinx::fem::FiniteElement> element1 = a.function_spaces().at(1)->element();
+        const std::function<void(const xtl::span<T> &,
+                                 const xtl::span<const std::uint32_t> &, std::int32_t,
+                                 int)>
+            apply_dof_transformation = element0->get_dof_transformation_function<T>();
+        const std::function<void(const xtl::span<T> &,
+                                 const xtl::span<const std::uint32_t> &, std::int32_t,
+                                 int)>
+            apply_dof_transformation_to_transpose = element1->get_dof_transformation_to_transpose_function<T>();
+
+        const bool needs_transformation_data = element0->needs_dof_transformations() or element1->needs_dof_transformations() or a.needs_facet_permutations();
+        xtl::span<const std::uint32_t> cell_info;
+        if (needs_transformation_data)
+        {
+            mesh->topology_mutable().create_entity_permutations();
+            cell_info = xtl::span(mesh->topology().get_cell_permutation_info());
+        }
     }
 }
