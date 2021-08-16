db96b683195917a1d20e5150dccb5f1856eb58cd
Joe Dean <jpd62@cam.ac.uk>
2021-08-16 12:03:43 +0100

    Create (incorrect) sparsity pattern

--- cpp/utils.cpp
+++ cpp/utils.cpp
@@ -1,1 +1,35 @@
 #include "utils.h"
+#include <array>
+#include <dolfinx/common/IndexMap.h>
+
+dolfinx::la::SparsityPattern dolfinx_hdg::fem::create_sparsity_pattern(
+    const dolfinx::mesh::Topology &topology,
+    const std::array<const std::reference_wrapper<const dolfinx::fem::DofMap>, 2> &
+        dofmaps)
+{
+    // Get common::IndexMaps for each dimension
+    const std::array index_maps{dofmaps[0].get().index_map,
+                                dofmaps[1].get().index_map};
+    const std::array bs = {dofmaps[0].get().index_map_bs(),
+                           dofmaps[1].get().index_map_bs()};
+
+    // Create and build sparsity pattern
+    assert(dofmaps[0].get().index_map);
+    dolfinx::la::SparsityPattern sp(
+        dofmaps[0].get().index_map->comm(
+            dolfinx::common::IndexMap::Direction::forward),
+        index_maps, bs);
+
+    const int D = topology.dim() - 1;
+    auto cells = topology.connectivity(D, 0);
+    assert(cells);
+    for (int c = 0; c < cells->num_nodes(); ++c)
+    {
+        sp.insert(dofmaps[0].get().cell_dofs(c),
+                  dofmaps[1].get().cell_dofs(c));
+    }
+
+    // TODO Assemble here or return unassembled?
+    sp.assemble();
+    return sp;
+}
--- cpp/utils.h
+++ cpp/utils.h
@@ -3,15 +3,39 @@
 #include <dolfinx/fem/Form.h>
 #include <dolfinx/la/SparsityPattern.h>
 #include <dolfinx/fem/utils.h>
+#include <dolfinx/fem/DofMap.h>
+#include <dolfinx/mesh/Topology.h>
 
 namespace dolfinx_hdg::fem
 {
+    dolfinx::la::SparsityPattern create_sparsity_pattern(
+    const dolfinx::mesh::Topology& topology,
+    const std::array<const std::reference_wrapper<const dolfinx::fem::DofMap>, 2>&
+        dofmaps);
+
     template <typename T>
     dolfinx::la::SparsityPattern create_sparsity_pattern(
         const dolfinx::fem::Form<T>& a)
     {
-        dolfinx::la::SparsityPattern sp = dolfinx::fem::create_sparsity_pattern(a);
-        sp.assemble();
-        return sp;
+        if (a.rank() != 2)
+        {
+            throw std::runtime_error(
+                "Cannot create sparsity pattern. Form is not a bilinear form");
+        }
+
+        // Get dof maps and mesh
+        std::array<const std::reference_wrapper<const dolfinx::fem::DofMap>, 2>
+            dofmaps{
+                *a.function_spaces().at(0)->dofmap(),
+                *a.function_spaces().at(1)->dofmap()};
+        std::shared_ptr mesh = a.mesh();
+        assert(mesh);
+
+        const int tdim = mesh->topology().dim();
+        mesh->topology_mutable().create_entities(tdim - 1);
+        // TODO Is this needed?
+        // mesh->topology_mutable().create_connectivity(tdim - 1, tdim);
+
+        return create_sparsity_pattern(mesh->topology(), dofmaps);
     }
 }
