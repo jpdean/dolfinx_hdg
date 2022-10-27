78b4491557df13ab2ce1c61fc7d13d9f1a63eaa2
Joe Dean <jpd62@cam.ac.uk>
2022-10-27 17:34:25 +0100

    Call assembler

--- cpp/assemble_matrix_impl.h
+++ cpp/assemble_matrix_impl.h
@@ -217,6 +217,118 @@ namespace dolfinx_hdg::fem::impl
     // }
 
     template <typename T, typename U>
+    void assemble_cells(
+        U mat_set, const mesh::Mesh& mesh,
+        const std::span<const std::int32_t> &cells,
+        const std::function<void(const std::span<T> &,
+                                 const std::span<const std::uint32_t> &,
+                                 std::int32_t, int)> &dof_transform,
+        const dolfinx::graph::AdjacencyList<std::int32_t> &dofmap0, int bs0,
+        const std::function<void(const std::span<T> &,
+                                 const std::span<const std::uint32_t> &,
+                                 std::int32_t, int)> &dof_transform_to_transpose,
+        const dolfinx::graph::AdjacencyList<std::int32_t> &dofmap1, int bs1,
+        const std::span<const std::int8_t> &bc0,
+        const std::span<const std::int8_t> &bc1,
+        const std::function<void(T *, const T *, const T *,
+                                 const dolfinx::fem::impl::scalar_value_type_t<T> *, const int *,
+                                 const std::uint8_t *)> &kernel,
+        const std::span<const T> &coeffs, int cstride,
+        const std::span<const T> &constants,
+        const std::span<const std::uint32_t> &cell_info_0,
+        const std::span<const std::uint32_t> &cell_info_1,
+        const std::function<std::int32_t(const std::span<const std::int32_t> &)> &
+            cell_map_0,
+        const std::function<std::int32_t(const std::span<const std::int32_t> &)> &
+            cell_map_1)
+    {
+        if (cells.empty())
+            return;
+
+        // Prepare cell geometry
+        const dolfinx::mesh::Geometry &geometry = mesh.geometry();
+        const graph::AdjacencyList<std::int32_t> &x_dofmap = geometry.dofmap();
+        const std::size_t num_dofs_g = geometry.cmap().dim();
+        std::span<const double> x_g = geometry.x();
+
+        // Iterate over active cells
+        const int num_dofs0 = dofmap0.links(0).size();
+        const int num_dofs1 = dofmap1.links(0).size();
+        const int ndim0 = bs0 * num_dofs0;
+        const int ndim1 = bs1 * num_dofs1;
+        // const int num_cell_facets = dolfinx::mesh::cell_num_entities(mesh.topology().cell_type(), mesh.topology().dim() - 1)
+        // std::vector<T> Ae(ndim0 * ndim1);
+        // const std::span<T> _Ae(Ae);
+        // std::vector<scalar_value_type_t<T>> coordinate_dofs(3 * num_dofs_g);
+
+        // // Iterate over active cells
+        // for (std::size_t index = 0; index < cells.size(); ++index)
+        // {
+        //     std::int32_t c = cells[index];
+        //     // Map the cell in the integration domain to the cell in the mesh
+        //     // each function space is defined over
+        //     std::int32_t c_0 = cell_map_0(cells.subspan(index, 1));
+        //     assert(c_0 >= 0);
+        //     std::int32_t c_1 = cell_map_1(cells.subspan(index, 1));
+        //     assert(c_1 >= 0);
+
+        //     // Get cell coordinates/geometry
+        //     auto x_dofs = x_dofmap.links(c);
+        //     for (std::size_t i = 0; i < x_dofs.size(); ++i)
+        //     {
+        //         std::copy_n(std::next(x_g.begin(), 3 * x_dofs[i]), 3,
+        //                     std::next(coordinate_dofs.begin(), 3 * i));
+        //     }
+
+        //     // Tabulate tensor
+        //     std::fill(Ae.begin(), Ae.end(), 0);
+        //     kernel(Ae.data(), coeffs.data() + index * cstride, constants.data(),
+        //            coordinate_dofs.data(), nullptr, nullptr);
+
+        //     dof_transform(_Ae, cell_info_1, c_1, ndim1);
+        //     dof_transform_to_transpose(_Ae, cell_info_0, c_0, ndim0);
+
+        //     // Zero rows/columns for essential bcs
+        //     auto dofs0 = dofmap0.links(c_0);
+        //     auto dofs1 = dofmap1.links(c_1);
+        //     if (!bc0.empty())
+        //     {
+        //         for (int i = 0; i < num_dofs0; ++i)
+        //         {
+        //             for (int k = 0; k < bs0; ++k)
+        //             {
+        //                 if (bc0[bs0 * dofs0[i] + k])
+        //                 {
+        //                     // Zero row bs0 * i + k
+        //                     const int row = bs0 * i + k;
+        //                     std::fill_n(std::next(Ae.begin(), ndim1 * row), ndim1, 0.0);
+        //                 }
+        //             }
+        //         }
+        //     }
+
+        //     if (!bc1.empty())
+        //     {
+        //         for (int j = 0; j < num_dofs1; ++j)
+        //         {
+        //             for (int k = 0; k < bs1; ++k)
+        //             {
+        //                 if (bc1[bs1 * dofs1[j] + k])
+        //                 {
+        //                     // Zero column bs1 * j + k
+        //                     const int col = bs1 * j + k;
+        //                     for (int row = 0; row < ndim0; ++row)
+        //                         Ae[row * ndim1 + col] = 0.0;
+        //                 }
+        //             }
+        //         }
+        //     }
+
+        //     mat_set(dofs0, dofs1, Ae);
+        // }
+    }
+
+    template <typename T, typename U>
     void assemble_matrix(
         U mat_set, const dolfinx::fem::Form<T> &a, const std::span<const T> &constants,
         const std::map<std::pair<dolfinx::fem::IntegralType, int>,
@@ -224,12 +336,10 @@ namespace dolfinx_hdg::fem::impl
         const std::span<const std::int8_t> &bc0,
         const std::span<const std::int8_t> &bc1)
     {
-        std::cout << "impl::assemble_matrix\n";
         std::shared_ptr<const dolfinx::mesh::Mesh> mesh = a.mesh();
         assert(mesh);
 
         // Get dofmap data
-        std::cout << "dofmaps\n";
         std::shared_ptr<const dolfinx::fem::DofMap> dofmap0 = a.function_spaces().at(0)->dofmap();
         std::shared_ptr<const dolfinx::fem::DofMap> dofmap1 = a.function_spaces().at(1)->dofmap();
         assert(dofmap0);
@@ -239,7 +349,6 @@ namespace dolfinx_hdg::fem::impl
         const dolfinx::graph::AdjacencyList<std::int32_t> &dofs1 = dofmap1->list();
         const int bs1 = dofmap1->bs();
 
-        std::cout << "eles\n";
         std::shared_ptr<const dolfinx::fem::FiniteElement> element0 = a.function_spaces().at(0)->element();
         std::shared_ptr<const dolfinx::fem::FiniteElement> element1 = a.function_spaces().at(1)->element();
         const std::function<void(const std::span<T> &,
@@ -265,23 +374,18 @@ namespace dolfinx_hdg::fem::impl
             cell_info_1 = std::span(mesh_1->topology().get_cell_permutation_info());
         }
 
-        std::cout << "ent maps\n";
         const auto entity_map_0 = a.function_space_to_entity_map(*a.function_spaces().at(0));
         const auto entity_map_1 = a.function_space_to_entity_map(*a.function_spaces().at(1));
 
-        std::cout << "for loop\n";
         for (int i : a.integral_ids(dolfinx::fem::IntegralType::cell))
         {
-            std::cout << "kernel\n";
             const auto &fn = a.kernel(dolfinx::fem::IntegralType::cell, i);
-            std::cout << "coeffs\n";
             const auto &[coeffs, cstride] = coefficients.at({dolfinx::fem::IntegralType::cell, i});
-            std::cout << "cells\n";
             const std::vector<std::int32_t> &cells = a.cell_domains(i);
-            // impl::assemble_cells(mat_set, mesh->geometry(), cells, dof_transform, dofs0,
-            //                      bs0, dof_transform_to_transpose, dofs1, bs1, bc0, bc1,
-            //                      fn, coeffs, cstride, constants, cell_info_0,
-            //                      cell_info_1, entity_map_0, entity_map_1);
+            impl::assemble_cells(mat_set, *mesh, cells, dof_transform, dofs0,
+                                 bs0, dof_transform_to_transpose, dofs1, bs1, bc0, bc1,
+                                 fn, coeffs, cstride, constants, cell_info_0,
+                                 cell_info_1, entity_map_0, entity_map_1);
         }
     }
 }
