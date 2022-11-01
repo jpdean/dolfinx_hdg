123df55b10360387d15f568a0ecd6bbd645016ec
Joe Dean <jpd62@cam.ac.uk>
2022-11-01 17:42:55 +0000

    Remove commented code

--- python/dolfinx_hdg/assemble.py
+++ python/dolfinx_hdg/assemble.py
@@ -7,41 +7,6 @@ from dolfinx.fem.forms import FormMetaClass
 import collections
 
 
-# @functools.singledispatch
-# def assemble_vector(L, mesh, facet_mesh,
-#                     coeffs=Coefficients(None, None)) -> PETSc.Vec:
-#     """Assemble linear form into a new PETSc vector. The returned vector is
-#     not finalised, i.e. ghost values are not accumulated on the owning
-#     processes.
-
-#     """
-#     b = dolfinx.la.create_petsc_vector(
-#         L.function_spaces[0].dofmap.index_map,
-#         L.function_spaces[0].dofmap.index_map_bs)
-#     c = (coeffs[0] if coeffs[0] is not None else pack_constants(L),
-#          coeffs[1] if coeffs[1] is not None else dolfinx_hdg.cpp.pack_coefficients(L))
-#     with b.localForm() as b_local:
-#         b_local.set(0.0)
-#         dolfinx_hdg.cpp.assemble_vector(
-#             b_local.array_w, L, mesh, facet_mesh, c[0], c[1])
-#     return b
-
-
-# @assemble_vector.register(PETSc.Vec)
-# def _(b: PETSc.Vec, L, mesh, facet_mesh,
-#       coeffs=Coefficients(None, None)) -> PETSc.Vec:
-#     """Assemble linear form into an existing PETSc vector. The vector is not
-#     zeroed before assembly and it is not finalised, qi.e. ghost values are
-#     not accumulated on the owning processes.
-#     """
-#     c = (coeffs[0] if coeffs[0] is not None else pack_constants(L),
-#          coeffs[1] if coeffs[1] is not None else dolfinx_hdg.cpp.pack_coefficients(L))
-#     with b.localForm() as b_local:
-#         dolfinx_hdg.cpp.assemble_vector(
-#             b_local.array_w, L, mesh, facet_mesh, c[0], c[1])
-#     return b
-
-
 def pack_coefficients(form):
     """Compute form coefficients.
 
@@ -300,25 +265,3 @@ def _assemble_vector_block_vec(b: PETSc.Vec, L, a, bcs=[], x0=None,
         offset += size
 
     return b
-
-# @assemble_matrix.register(PETSc.Mat)
-# def _(A: PETSc.Mat,
-#       a,
-#       mesh, facet_mesh,
-#       bcs = [],
-#       diagonal: float = 1.0,
-#       coeffs=Coefficients(None, None)) -> PETSc.Mat:
-#     """Assemble bilinear form into a matrix. The returned matrix is not
-#     finalised, i.e. ghost values are not accumulated.
-
-#     """
-#     c = (coeffs[0] if coeffs[0] is not None else pack_constants(a),
-#          coeffs[1] if coeffs[1] is not None else dolfinx_hdg.cpp.pack_coefficients(a))
-#     dolfinx_hdg.cpp.assemble_matrix_petsc(A, a, mesh, facet_mesh, c[0], c[1], bcs)
-#     # TODO When will this not be true? Mixed facet HDG terms?
-#     if a.function_spaces[0].id == a.function_spaces[1].id:
-#         A.assemblyBegin(PETSc.Mat.AssemblyType.FLUSH)
-#         A.assemblyEnd(PETSc.Mat.AssemblyType.FLUSH)
-#         dolfinx.cpp.fem.petsc.insert_diagonal(A, a.function_spaces[0],
-#                                               bcs, diagonal)
-#     return A
--- python/dolfinx_hdg/wrapper.cpp
+++ python/dolfinx_hdg/wrapper.cpp
@@ -3,13 +3,10 @@
 #include <pybind11/numpy.h>
 #include <dolfinx_hdg/utils.h>
 #include <dolfinx_hdg/petsc.h>
-// #include <petscmat.h>
-// #include <dolfinx/fem/DirichletBC.h>
+#include <dolfinx/fem/DirichletBC.h>
 #include <dolfinx_hdg/assembler.h>
 #include <dolfinx/la/petsc.h>
-// #include <iostream>
 #include "caster_petsc.h"
-// #include <dolfinx_hdg/Form.h>
 
 // FIXME Include this from dolfinx wrappers
 namespace dolfinx_hdg_wrappers
@@ -157,93 +154,4 @@ PYBIND11_MODULE(cpp, m)
     dolfinx_hdg_wrappers::declare_functions<float>(m);
     dolfinx_hdg_wrappers::declare_functions<std::complex<double>>(m);
     dolfinx_hdg_wrappers::declare_functions<std::complex<float>>(m);
-
-    // m.def("create_sparsity_pattern",
-    //       &dolfinx_hdg::fem::create_sparsity_pattern<PetscScalar>,
-    //       "Create a sparsity pattern for bilinear form.");
-
-    // m.def("assemble_matrix_petsc",
-    //       [](Mat A, const dolfinx::fem::Form<PetscScalar>& a,
-    //          const dolfinx::mesh::Mesh& mesh,
-    //          const dolfinx::mesh::Mesh& facet_mesh,
-    //          const py::array_t<PetscScalar, py::array::c_style>& constants,
-    //          const std::map<std::pair<dolfinx::fem::IntegralType, int>,
-    //                         py::array_t<PetscScalar, py::array::c_style>>&
-    //             coefficients,
-    //          const std::vector<std::shared_ptr<
-    //                           const dolfinx::fem::DirichletBC<PetscScalar>>>& bcs)
-    //       {
-    //         auto _coefficients = dolfinx_hdg_wrappers::py_to_cpp_coeffs(coefficients);
-    //         dolfinx_hdg::fem::assemble_matrix(
-    //               dolfinx::la::petsc::Matrix::set_block_fn(A, ADD_VALUES), a,
-    //               mesh, facet_mesh,
-    //               xtl::span(constants), _coefficients, bcs);
-    //       });
-
-    // TODO / FIXME dolfinx uses templating for generic type rather than PetscScalar
-    // here (and elsewhere). Add this.
-    // m.def(
-    //     "assemble_vector",
-    //     [](pybind11::array_t<PetscScalar, pybind11::array::c_style> b,
-    //        const dolfinx::fem::Form<PetscScalar> &L,
-    //        const dolfinx::mesh::Mesh& mesh,
-    //        const dolfinx::mesh::Mesh& facet_mesh,
-    //        const py::array_t<PetscScalar, py::array::c_style>& constants,
-    //        const std::map<std::pair<dolfinx::fem::IntegralType, int>,
-    //                     py::array_t<PetscScalar, py::array::c_style>>& coefficients)
-    //     {
-    //         auto _coefficients = dolfinx_hdg_wrappers::py_to_cpp_coeffs(coefficients);
-    //         dolfinx_hdg::fem::assemble_vector<PetscScalar>(
-    //             xtl::span(b.mutable_data(), b.size()), L, mesh,
-    //             facet_mesh, constants, _coefficients);
-    //     },
-    //     pybind11::arg("b"), pybind11::arg("L"), py::arg("mesh"),
-    //     py::arg("facet_mesh"), py::arg("constants"), py::arg("coeffs"),
-    //     "Assemble linear form into an existing vector");
-
-    // m.def(
-    //   "pack_coefficients",
-    //   [](const dolfinx::fem::Form<PetscScalar>& form)
-    //   {
-    //     using Key_t = typename std::pair<dolfinx::fem::IntegralType, int>;
-
-    //     // Pack coefficients
-    //     std::map<Key_t, std::pair<std::vector<PetscScalar>, int>> coeffs
-    //         = dolfinx_hdg::fem::pack_coefficients(form);
-
-    //     // Move into NumPy data structures
-    //     std::map<Key_t, py::array_t<PetscScalar, py::array::c_style>> c;
-    //     std::transform(
-    //         coeffs.begin(), coeffs.end(), std::inserter(c, c.end()),
-    //         [](auto& e) -> typename decltype(c)::value_type
-    //         {
-    //           int num_ents = e.second.first.empty()
-    //                              ? 0
-    //                              : e.second.first.size() / e.second.second;
-    //           return {e.first,
-    //                   dolfinx_hdg_wrappers::as_pyarray(std::move(e.second.first),
-    //                              std::array{num_ents, e.second.second})};
-    //         });
-    //     return c;
-    //   },
-    //   "Pack coefficients for a Form.");
-
-    // m.def(
-    //     "back_sub",
-    //     [](pybind11::array_t<PetscScalar, pybind11::array::c_style> x,
-    //        pybind11::array_t<PetscScalar, pybind11::array::c_style> xbar,
-    //        const std::vector<std::vector<std::shared_ptr<
-    //            const dolfinx::fem::Form<PetscScalar>>>> &a,
-    //        const std::vector<std::shared_ptr<
-    //            const dolfinx::fem::Form<PetscScalar>>> &L)
-    //     {
-    //         // FIXME xbar is constant. How do I pass it as a const?
-    //         dolfinx_hdg::sc::back_sub<PetscScalar>(
-    //             xtl::span(x.mutable_data(), x.size()),
-    //             xtl::span(xbar.mutable_data(), xbar.size()),
-    //             a, L);
-    //     },
-    //     pybind11::arg("x"), pybind11::arg("xbar"), pybind11::arg("a"),
-    //     pybind11::arg("L"),
-    //     "Perform backsubstitution");
 }
