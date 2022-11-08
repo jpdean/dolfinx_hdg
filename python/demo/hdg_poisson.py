fbfce7e41985761cf8cb19dbaaa1187852de7abf
Joe Dean <jpd62@cam.ac.uk>
2022-11-08 16:25:36 +0000

    Timings

--- python/demo/hdg_poisson.py
+++ python/demo/hdg_poisson.py
@@ -18,6 +18,7 @@ from dolfinx_hdg.assemble import assemble_vector as assemble_vector_hdg
 from dolfinx_hdg.assemble import pack_coefficients
 from dolfinx_hdg.assemble import apply_lifting as apply_lifting_hdg
 from utils import reorder_mesh, norm_L2
+import json
 
 
 def main():
@@ -37,116 +38,121 @@ def main():
             print(string)
             sys.stdout.flush()
 
+    def print_and_time(name):
+        par_print(name)
+        return Timer(name)
+
     def u_e(x, module=np):
         u_e = 1
         for i in range(tdim):
             u_e *= module.cos(module.pi * x[i])
         return u_e
 
+    total_timer = Timer("TOTAL")
     comm = MPI.COMM_WORLD
 
-    par_print("Create mesh")
-    with Timer("Create mesh") as t:
-        # n = 8
-        n = round((500000 * comm.size / 60)**(1 / 3))
-        par_print(f"n = {n}")
-        # msh = mesh.create_unit_square(
-        #     comm, n, n, ghost_mode=mesh.GhostMode.none)
-        msh = mesh.create_unit_cube(
-            comm, n, n, n, ghost_mode=mesh.GhostMode.none)
-        reorder_mesh(msh)
+    timings = {}
+    n = 8
+    timer = print_and_time(f"Create mesh (n = {n})")
+    # n = round((500000 * comm.size / 60)**(1 / 3))
+    msh = mesh.create_unit_square(
+        comm, n, n, ghost_mode=mesh.GhostMode.none)
+    # msh = mesh.create_unit_cube(
+    #     comm, n, n, n, ghost_mode=mesh.GhostMode.none)
+    timings["create_mesh"] = timer.stop()
 
-    par_print("Create submesh")
-    with Timer("Create submesh") as t:
-        tdim = msh.topology.dim
-        fdim = tdim - 1
-        num_cell_facets = cell_num_entities(msh.topology.cell_type, fdim)
-        msh.topology.create_entities(fdim)
-        facet_imap = msh.topology.index_map(fdim)
-        num_facets = facet_imap.size_local + facet_imap.num_ghosts
-        facets = np.arange(num_facets, dtype=np.int32)
+    timer = print_and_time("Reorder mesh")
+    reorder_mesh(msh)
+    timings["reorder_mesh"] = timer.stop()
 
-        # NOTE Despite all facets being present in the submesh, the entity map isn't
-        # necessarily the identity in parallel
-        facet_mesh, entity_map = mesh.create_submesh(msh, fdim, facets)[0:2]
+    timer = print_and_time("Create facet mesh")
+    tdim = msh.topology.dim
+    fdim = tdim - 1
+    num_cell_facets = cell_num_entities(msh.topology.cell_type, fdim)
+    msh.topology.create_entities(fdim)
+    facet_imap = msh.topology.index_map(fdim)
+    num_facets = facet_imap.size_local + facet_imap.num_ghosts
+    facets = np.arange(num_facets, dtype=np.int32)
 
-    par_print("Create function spaces")
-    with Timer("Create function spaces") as t:
-        k = 1
-        V = fem.FunctionSpace(msh, ("Discontinuous Lagrange", k))
-        Vbar = fem.FunctionSpace(facet_mesh, ("Discontinuous Lagrange", k))
+    # NOTE Despite all facets being present in the submesh, the entity map isn't
+    # necessarily the identity in parallel
+    facet_mesh, entity_map = mesh.create_submesh(msh, fdim, facets)[0:2]
+    timings["create_facet_mesh"] = timer.stop()
+
+    timer = print_and_time("Create function spaces")
+    k = 1
+    V = fem.FunctionSpace(msh, ("Discontinuous Lagrange", k))
+    Vbar = fem.FunctionSpace(facet_mesh, ("Discontinuous Lagrange", k))
 
     V_ele_space_dim = V.element.space_dimension
     Vbar_ele_space_dim = Vbar.element.space_dimension
 
     num_cell_facets = msh.ufl_cell().num_facets()
     num_dofs_g = len(msh.geometry.dofmap.links(0))
+    timings["create_function_spaces"] = timer.stop()
 
-    par_print("Define problem")
-    with Timer("Define problem") as t:
-        u = ufl.TrialFunction(V)
-        v = ufl.TestFunction(V)
-        ubar = ufl.TrialFunction(Vbar)
-        vbar = ufl.TestFunction(Vbar)
+    timer = print_and_time("Define problem")
+    u = ufl.TrialFunction(V)
+    v = ufl.TestFunction(V)
+    ubar = ufl.TrialFunction(Vbar)
+    vbar = ufl.TestFunction(Vbar)
 
-        h = ufl.CellDiameter(msh)
-        n = ufl.FacetNormal(msh)
-        gamma = 16.0 * k**2 / h
+    h = ufl.CellDiameter(msh)
+    n = ufl.FacetNormal(msh)
+    gamma = 16.0 * k**2 / h
 
-        dx_c = ufl.Measure("dx", domain=msh)
-        ds_c = ufl.Measure("ds", domain=msh)
+    dx_c = ufl.Measure("dx", domain=msh)
+    ds_c = ufl.Measure("ds", domain=msh)
 
-        a_00 = inner(grad(u), grad(v)) * dx_c - \
-            (inner(u, dot(grad(v), n)) * ds_c +
-             inner(v, dot(grad(u), n)) * ds_c) + \
-            gamma * inner(u, v) * ds_c
-        a_10 = inner(dot(grad(u), n) - gamma * u, vbar) * ds_c
-        a_11 = gamma * inner(ubar, vbar) * ds_c
+    a_00 = inner(grad(u), grad(v)) * dx_c - \
+        (inner(u, dot(grad(v), n)) * ds_c +
+            inner(v, dot(grad(u), n)) * ds_c) + \
+        gamma * inner(u, v) * ds_c
+    a_10 = inner(dot(grad(u), n) - gamma * u, vbar) * ds_c
+    a_11 = gamma * inner(ubar, vbar) * ds_c
 
-        x = ufl.SpatialCoordinate(msh)
-        f = - div(grad(u_e(x, ufl)))
-        L_0 = inner(f, v) * dx_c
+    x = ufl.SpatialCoordinate(msh)
+    f = - div(grad(u_e(x, ufl)))
+    L_0 = inner(f, v) * dx_c
+    timings["define_problem"] = timer.stop()
 
-    par_print("Create inv ent map")
-    with Timer("Create inv ent map") as t:
-        inv_entity_map = np.full_like(entity_map, -1)
-        for i, f in enumerate(entity_map):
-            inv_entity_map[f] = i
+    timer = print_and_time("Create inverse entity map")
+    inv_entity_map = np.full_like(entity_map, -1)
+    for i, f in enumerate(entity_map):
+        inv_entity_map[f] = i
+    timings["create_inv_ent_map"] = timer.stop()
 
+    timer = print_and_time("JIT kernels")
     nptype = "float64"
     ffcxtype = "double"
 
-    par_print("JIT kernels")
-    with Timer("JIT kernels") as t:
-        # LHS forms
-        ufcx_form_00, _, _ = jit.ffcx_jit(
-            msh.comm, a_00, form_compiler_options={"scalar_type": ffcxtype})
-        kernel_00_cell = getattr(ufcx_form_00.integrals(IntegralType.cell)[0],
-                                 f"tabulate_tensor_{nptype}")
-        kernel_00_facet = getattr(ufcx_form_00.integrals(IntegralType.exterior_facet)[0],
-                                  f"tabulate_tensor_{nptype}")
-        ufcx_form_10, _, _ = jit.ffcx_jit(
-            msh.comm, a_10, form_compiler_options={"scalar_type": ffcxtype})
-        kernel_10 = getattr(ufcx_form_10.integrals(IntegralType.exterior_facet)[0],
-                            f"tabulate_tensor_{nptype}")
-        ufcx_form_11, _, _ = jit.ffcx_jit(
-            msh.comm, a_11, form_compiler_options={"scalar_type": ffcxtype})
-        kernel_11 = getattr(ufcx_form_11.integrals(IntegralType.exterior_facet)[0],
-                            f"tabulate_tensor_{nptype}")
+    # LHS forms
+    ufcx_form_00, _, _ = jit.ffcx_jit(
+        msh.comm, a_00, form_compiler_options={"scalar_type": ffcxtype})
+    kernel_00_cell = getattr(ufcx_form_00.integrals(IntegralType.cell)[0],
+                                f"tabulate_tensor_{nptype}")
+    kernel_00_facet = getattr(ufcx_form_00.integrals(IntegralType.exterior_facet)[0],
+                                f"tabulate_tensor_{nptype}")
+    ufcx_form_10, _, _ = jit.ffcx_jit(
+        msh.comm, a_10, form_compiler_options={"scalar_type": ffcxtype})
+    kernel_10 = getattr(ufcx_form_10.integrals(IntegralType.exterior_facet)[0],
+                        f"tabulate_tensor_{nptype}")
+    ufcx_form_11, _, _ = jit.ffcx_jit(
+        msh.comm, a_11, form_compiler_options={"scalar_type": ffcxtype})
+    kernel_11 = getattr(ufcx_form_11.integrals(IntegralType.exterior_facet)[0],
+                        f"tabulate_tensor_{nptype}")
 
-        # RHS forms
-        ufcx_form_0, _, _ = jit.ffcx_jit(
-            msh.comm, L_0, form_compiler_options={"scalar_type": ffcxtype})
-        kernel_0 = getattr(ufcx_form_0.integrals(IntegralType.cell)[0],
-                           f"tabulate_tensor_{nptype}")
+    # RHS forms
+    ufcx_form_0, _, _ = jit.ffcx_jit(
+        msh.comm, L_0, form_compiler_options={"scalar_type": ffcxtype})
+    kernel_0 = getattr(ufcx_form_0.integrals(IntegralType.cell)[0],
+                        f"tabulate_tensor_{nptype}")
 
-    par_print("FFI setup")
-    with Timer("FFI setup") as t:
-        ffi = cffi.FFI()
-        # FIXME See if there is a better way to pass null
-        null64 = np.zeros(0, dtype=np.float64)
-        null32 = np.zeros(0, dtype=np.int32)
-        null8 = np.zeros(0, dtype=np.uint8)
+    ffi = cffi.FFI()
+    # FIXME See if there is a better way to pass null
+    null64 = np.zeros(0, dtype=np.float64)
+    null32 = np.zeros(0, dtype=np.int32)
+    null8 = np.zeros(0, dtype=np.uint8)
 
     @numba.njit(fastmath=True)
     def compute_A11(coords):
@@ -261,123 +267,142 @@ def main():
 
         x += np.linalg.solve(A00, b_0 - A10.T @ u_bar)
 
-    par_print("BC")
-    with Timer("BC") as t:
-        msh_boundary_facets = mesh.locate_entities_boundary(
-            msh, fdim, boundary)
-        facet_mesh_boundary_facets = inv_entity_map[msh_boundary_facets]
-        bc_dofs = fem.locate_dofs_topological(
-            Vbar, fdim, facet_mesh_boundary_facets)
-        u_bc = fem.Function(Vbar)
-        u_bc.interpolate(u_e)
-        bc = fem.dirichletbc(u_bc, bc_dofs)
+    timings["jit_kernels"] = timer.stop()
 
-    par_print("Assemble mat")
-    with Timer("Assemble mat") as t:
-        integrals_a = {
-            fem.IntegralType.cell: {-1: (tabulate_tensor_a.address, [])}}
-        a = Form_float64(
-            [Vbar._cpp_object, Vbar._cpp_object], integrals_a, [], [], False, msh,
-            entity_maps={facet_mesh: inv_entity_map})
-        A = assemble_matrix_hdg(a, bcs=[bc])
-        A.assemble()
+    timer = print_and_time("Boundary conditions")
+    msh_boundary_facets = mesh.locate_entities_boundary(
+        msh, fdim, boundary)
+    facet_mesh_boundary_facets = inv_entity_map[msh_boundary_facets]
+    bc_dofs = fem.locate_dofs_topological(
+        Vbar, fdim, facet_mesh_boundary_facets)
+    u_bc = fem.Function(Vbar)
+    u_bc.interpolate(u_e)
+    bc = fem.dirichletbc(u_bc, bc_dofs)
+    timings["bcs"] = timer.stop()
 
-    par_print("Assemble vec")
-    with Timer("Assemble vec") as t:
-        integrals_L = {
-            fem.IntegralType.cell: {-1: (tabulate_tensor_L.address, [])}}
-        L = Form_float64(
-            [Vbar._cpp_object], integrals_L, [], [], False, msh,
-            entity_maps={facet_mesh: inv_entity_map})
-        b = assemble_vector_hdg(L)
-        apply_lifting_hdg(b, [a], bcs=[[bc]])
-        b.ghostUpdate(addv=PETSc.InsertMode.ADD,
-                      mode=PETSc.ScatterMode.REVERSE)
-        fem.petsc.set_bc(b, [bc])
+    timer = print_and_time("Assemble matrix")
+    integrals_a = {
+        fem.IntegralType.cell: {-1: (tabulate_tensor_a.address, [])}}
+    a = Form_float64(
+        [Vbar._cpp_object, Vbar._cpp_object], integrals_a, [], [], False, msh,
+        entity_maps={facet_mesh: inv_entity_map})
+    A = assemble_matrix_hdg(a, bcs=[bc])
+    A.assemble()
+    timings["assemble_vec"] = timer.stop()
 
-    par_print("Setup solver")
-    with Timer("Setup solver") as t:
-        use_direct_solver = False
+    timer = print_and_time("Assemble vector")
+    integrals_L = {
+        fem.IntegralType.cell: {-1: (tabulate_tensor_L.address, [])}}
+    L = Form_float64(
+        [Vbar._cpp_object], integrals_L, [], [], False, msh,
+        entity_maps={facet_mesh: inv_entity_map})
+    b = assemble_vector_hdg(L)
+    apply_lifting_hdg(b, [a], bcs=[[bc]])
+    b.ghostUpdate(addv=PETSc.InsertMode.ADD,
+                    mode=PETSc.ScatterMode.REVERSE)
+    fem.petsc.set_bc(b, [bc])
+    timings["assemble_vec"] = timer.stop()
 
-        if use_direct_solver:
-            ksp = PETSc.KSP().create(msh.comm)
-            ksp.setOperators(A)
-            ksp.setType("preonly")
-            ksp.getPC().setType("lu")
-            ksp.getPC().setFactorSolverType("superlu_dist")
-        else:
-            opts = PETSc.Options()
-            # Solver (Use conjugate gradient)
-            opts['ksp_type'] = 'cg'
-            opts['ksp_rtol'] = '1.0e-8'
-            opts['ksp_view'] = None
-            opts['ksp_monitor'] = None
-            opts['pc_type'] = 'hypre'
-            opts['pc_hypre_type'] = 'boomeramg'
-            opts['pc_hypre_boomeramg_strong_threshold'] = 0.85
-            opts['pc_hypre_boomeramg_agg_nl'] = 1
-            # opts['pc_hypre_boomeramg_agg_num_paths'] = 2
+    timer = print_and_time("Setup solver")
+    use_direct_solver = False
 
-            PETSc.Options().view()
+    if use_direct_solver:
+        ksp = PETSc.KSP().create(msh.comm)
+        ksp.setOperators(A)
+        ksp.setType("preonly")
+        ksp.getPC().setType("lu")
+        ksp.getPC().setFactorSolverType("superlu_dist")
+    else:
+        opts = PETSc.Options()
+        # Solver (Use conjugate gradient)
+        opts['ksp_type'] = 'cg'
+        opts['ksp_rtol'] = '1.0e-8'
+        opts['ksp_view'] = None
+        opts['ksp_monitor'] = None
+        opts['pc_type'] = 'hypre'
+        opts['pc_hypre_type'] = 'boomeramg'
+        opts['pc_hypre_boomeramg_strong_threshold'] = 0.85
+        opts['pc_hypre_boomeramg_agg_nl'] = 1
+        # opts['pc_hypre_boomeramg_agg_num_paths'] = 2
 
-            ksp = PETSc.KSP().create(msh.comm)
-            ksp.setOperators(A)
-            ksp.setFromOptions()
+        PETSc.Options().view()
 
-    par_print("Solve")
-    with Timer("Solve") as t:
-        # Compute solution
-        ubar = fem.Function(Vbar)
-        ksp.solve(b, ubar.vector)
-        ubar.x.scatter_forward()
+        ksp = PETSc.KSP().create(msh.comm)
+        ksp.setOperators(A)
+        ksp.setFromOptions()
+
+    timings["setup_solver"] = timer.stop()
+
+    timer = print_and_time("Solve")
+    # Compute solution
+    ubar = fem.Function(Vbar)
+    ksp.solve(b, ubar.vector)
+    ubar.x.scatter_forward()
+    timings["solve"] = timer.stop()
 
     # par_print("Write")
 
     # with io.VTXWriter(msh.comm, "ubar.bp", ubar) as f:
     #     f.write(0.0)
 
-    par_print("Create backsub form")
-    with Timer("Create backsub form") as t:
-        # TODO Check with custom integration entities that this actually runs
-        # over all cells
-        integrals = {fem.IntegralType.cell: {-1: (backsub.address, [])}}
-        u_form = Form_float64([V._cpp_object], integrals,
-                              [ubar._cpp_object], [], False, None,
-                              entity_maps={facet_mesh: inv_entity_map})
+    timer = print_and_time("Backsubstitution")
+    # TODO Check with custom integration entities that this actually runs
+    # over all cells
+    integrals = {fem.IntegralType.cell: {-1: (backsub.address, [])}}
+    u_form = Form_float64([V._cpp_object], integrals,
+                            [ubar._cpp_object], [], False, None,
+                            entity_maps={facet_mesh: inv_entity_map})
+    coeffs = pack_coefficients(u_form)
 
-    par_print("Pack coeffs")
-    with Timer("Pack coeffs") as t:
-        coeffs = pack_coefficients(u_form)
-
-    par_print("Backsub")
-    with Timer("Backsub") as t:
-        u = fem.Function(V)
-        fem.assemble_vector(u.x.array, u_form, coeffs=coeffs)
-        u.vector.ghostUpdate(addv=PETSc.InsertMode.ADD,
-                             mode=PETSc.ScatterMode.REVERSE)
+    u = fem.Function(V)
+    fem.assemble_vector(u.x.array, u_form, coeffs=coeffs)
+    u.vector.ghostUpdate(addv=PETSc.InsertMode.ADD,
+                            mode=PETSc.ScatterMode.REVERSE)
+    timings["backsub"] = timer.stop()
 
     # par_print("Write")
     # with io.VTXWriter(msh.comm, "u.bp", u) as f:
     #     f.write(0.0)
 
-    par_print("Compute error")
-    with Timer("Compute error") as t:
-        e_L2 = norm_L2(msh.comm, u - u_e(x, ufl))
+    timer = print_and_time("Compute erorrs")
+    e_L2 = norm_L2(msh.comm, u - u_e(x, ufl))
+    timings["compute_errors_cell"] = timer.stop()
+
+    timings["total"] = total_timer.stop()
+
     if msh.comm.rank == 0:
         print(f"e_L2 = {e_L2}")
 
-    par_print(f"total num dofs V = {V.dofmap.index_map.size_global}")
-    par_print(f"total num dofs Vbar = {Vbar.dofmap.index_map.size_global}")
-    par_print(
-        f"total dofs = {V.dofmap.index_map.size_global + Vbar.dofmap.index_map.size_global}")
-    par_print(
-        f"total dofs per process = {(V.dofmap.index_map.size_global + Vbar.dofmap.index_map.size_global) / comm.size}")
+    num_cells = msh.topology.index_map(tdim).size_global
+    num_dofs_V = V.dofmap.index_map.size_global * V.dofmap.index_map_bs
+    num_dofs_Vbar = Vbar.dofmap.index_map.size_global * Vbar.dofmap.index_map_bs
+    total_dofs = num_dofs_V + num_dofs_Vbar
 
+    data = {}
+    data["num_proc"] = comm.size
+    data["num_cells"] = num_cells
+    data["num_dofs_V"] = num_dofs_V
+    data["num_dofs_Vbar"] = num_dofs_Vbar
+    data["total_dofs"] = total_dofs
+    data["e_u"] = e_L2
+    data["its"] = ksp.its
+
+    results = {}
+    results["data"] = data
+    results["timings"] = {}
+    for name, t in timings.items():
+        results["timings"][name] = comm.allreduce(t, op=MPI.MAX)
+
+    for name, val in results["data"].items():
+        par_print(f"{name} = {val}")
+
+    if comm.rank == 0:
+        with open(f"results_{comm.size}.json", "w") as f:
+            json.dump(results, f)
 
 if __name__ == "__main__":
     # import cProfile
     # cProfile.run(
     #     "main()", filename=f"out_python_{MPI.COMM_WORLD.rank}.profile")
-    with Timer("TOTAL") as t:
-        main()
+    main()
     list_timings(MPI.COMM_WORLD, [TimingType.wall, TimingType.user])
