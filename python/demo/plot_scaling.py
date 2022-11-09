4afb124c8c3821bbb4ade3aa6c02feca98c2278f
Joe Dean <jpd62@cam.ac.uk>
2022-11-09 11:48:05 +0000

    Add plots

--- python/demo/plot_scaling.py
+++ python/demo/plot_scaling.py
@@ -3,11 +3,11 @@ import json
 import matplotlib.pyplot as plt
 
 # TODO Read from file instead
-num_procs = [76 * i for i in range(1, 6)]
+num_procs = [2**i * 76 for i in range(6)]
 
 results = {}
 for num_proc in num_procs:
-    with open(f"results_{num_proc}.json", "r") as f:
+    with open(f"results/stokes/results_{num_proc}.json", "r") as f:
         results_dict = json.load(f)
 
     for dict in results_dict.values():
@@ -19,18 +19,19 @@ for num_proc in num_procs:
 
 results = pd.DataFrame(results)
 # print(results)
+# print(results.columns)
 
 x = "num_proc"
 results[[x, "its"]].plot.bar(x=x)
 plt.tight_layout()
 plt.show()
 
-results[[x, "assemble_mat", "assemble_pre", "assemble_vec", "create_function_spaces",
-         "create_facet_mesh", "backsub"]].plot.bar(x=x, stacked=True)
+results[[x, "assemble_mat", "assemble_vec",
+         "backsub", "assemble_pre"]].plot.bar(x=x, stacked=True)
 plt.tight_layout()
 plt.show()
 
-x = "num_proc"
-results[[x, "total"]].plot.bar(x=x)
+results["other"] = results["total"] - results["solve"]
+results[[x, "solve", "other"]].plot.bar(x=x, stacked=True)
 plt.tight_layout()
 plt.show()
